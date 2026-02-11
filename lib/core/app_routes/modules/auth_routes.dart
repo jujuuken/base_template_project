989ca33923d06056116f397f14fd3638b6e13dcc
Jujuu Ken <108442248+jujuuken@users.noreply.github.com>
2026-02-11 11:36:03 +0700

    Add splash screen route to AuthRoutes

--- lib/core/app_routes/modules/auth_routes.dart
+++ lib/core/app_routes/modules/auth_routes.dart
@@ -1,10 +1,16 @@
 import 'package:go_router/go_router.dart';
 import '../../../features/authentication/presentation/auth_screen/ui/login_screen.dart';
 import '../../../features/authentication/presentation/auth_screen/ui/register_screen.dart';
+import '../../../features/splash/presentation/pages/splash_screen.dart';
 import '../route_names.dart';
 
 class AuthRoutes {
   static List<GoRoute> routes = [
+    GoRoute(
+      path: RouteNames.splash.toPath,
+      name: RouteNames.splash,
+      builder: (context, state) => const SplashScreen(),
+    ),
     GoRoute(
       path: RouteNames.login.toPath,
       name: RouteNames.login,
